67e5e4f9a9565123f1ca4fbefb6b1e933fa2f5a9
NicatHuseyn <nicat5075@gmail.com>
2024-12-10 12:45:14 -0800

    IEnumerable interface and IEnumerator interface

---
+++ Iterative_Class_And_Yield_Keyword/Iterative_Class_And_Yield_Keyword/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Collections;
+
+Stock stock = new Stock();
+
+foreach (var item in stock)
+{
+    Console.WriteLine(item);    
+}
+
+class Stock
+{
+    List<string> products = new() { "computer", "phone", "TV", "watch"};
+    public void Add(string product)=> products.Add(product);
+
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        return products.GetEnumerator();
+    }
+}
+
